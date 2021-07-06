9904ade1c12273c9f67859ad9432da1eda23eafb
Ashley Kennedy <ashleytkennedy10@gmail.com>
2021-07-06 19:03:07 +0100

    added tailwind

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preconnect" href="https://app.snipcart.com" />
+					<link rel="preconnect" href="https://cdn.snipcart.com" />
+					<link
+						rel="stylesheet"
+						href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+				<script
+					async
+					src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js"
+				/>
+				<div
+					hidden
+					id="snipcart"
+					data-api-key="YThjODg0NDEtZGJiZi00NDZkLWE0YjAtOTY0NGI1MWUxOWI4NjM3NjA5NDgxMDk2NDc5NTk0"
+				/>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import products from "../products.json";
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
-				<link rel="preconnect" href="https://app.snipcart.com" />
-				<link rel="preconnect" href="https://cdn.snipcart.com" />
-				<link
-					rel="stylesheet"
-					href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css"
-				/>
 				<title>Techii</title>
 				<meta name="description" content="Techii Store" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}>
-				<p className={styles.description}>
+			<main>
+				<h1>Snipcart Test</h1>
+				<p>
 					<a
 						className="snipcart-checkout snipcart-summary"
 						href="#"
@@ -29,10 +23,10 @@ export default function Home() {
 						<span className="snipcart-total-price">$0.00</span>
 					</a>
 				</p>
-				<div className={styles.grid}>
+				<div>
 					{products.map((product) => {
 						return (
-							<div key={product.id} className={styles.card}>
+							<div key={product.id}>
 								<Image
 									src={product.image}
 									alt={`Preview of ${product.title}`}
@@ -59,28 +53,6 @@ export default function Home() {
 					})}
 				</div>
 			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-					</span>
-				</a>
-			</footer>
-			<script
-				async
-				src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js"
-			/>
-			<div
-				hidden
-				id="snipcart"
-				data-api-key="YThjODg0NDEtZGJiZi00NDZkLWE0YjAtOTY0NGI1MWUxOWI4NjM3NjA5NDgxMDk2NDc5NTk0"
-			/>
 		</div>
 	);
 }
